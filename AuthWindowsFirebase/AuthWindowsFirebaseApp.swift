b5c71babfa1db55d1449697bc2f130799cc6df6d
BB <izum_music@icloud.com>
2022-05-14 21:24:57 +0300

    Initial Commit

---
+++ AuthWindowsFirebase/AuthWindowsFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthWindowsFirebaseApp.swift
+//  AuthWindowsFirebase
+//
+//  Created by Artem Vinogradov on 14.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct AuthWindowsFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
